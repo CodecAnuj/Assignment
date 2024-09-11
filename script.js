64151c823abefc799a61cdd02b1f4873a98057fe
CodecAnuj <anujtiwari9111@gmail.com>
2024-09-11 08:33:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    let email =  document.getElementById('email').value;
+    let password =  document.getElementById('password').value;
+    const alertDiv =  document.getElementById('alert');
+
+    if (email === '' || password === '') {
+        alertDiv.textContent = 'Please fill both email and password!';
+      } else {
+        alertDiv.textContent = '';
+        window.location.href = "https://www.google.com/";
+      }
+}
